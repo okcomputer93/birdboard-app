92a00ceac7cd338a1dfe21e72e409f96e448fb26
Omar J <okcomputer2393@gmail.com>
2021-01-25 20:16:16 -0600

    First commit, first test, an user can create a project

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Projects;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Projects::all();
+        return view('projects.index', compact('projects'));
+
+    }
+    public function store()
+    {
+        Projects::create(request(['title', 'description']));
+        return redirect('/projects');
+    }
+}
