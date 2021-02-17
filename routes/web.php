4dd261b585e01aa85949939ff409c972ad534c23
Omar J <okcomputer2393@gmail.com>
2021-02-17 10:25:57 -0600

    Moving to resource projects routes

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::group(['middleware' => 'auth'], function (){
-    Route::get('/projects', [ProjectsController::class, 'index']);
-    Route::get('/projects/create', [ProjectsController::class, 'create']);
-    Route::get('/projects/{project}', [ProjectsController::class, 'show'])->name('projects.show');
-    Route::get('/projects/{project}/edit', [ProjectsController::class, 'edit']);
-    Route::patch('/projects/{project}', [ProjectsController::class, 'update']);
+    Route::resource('projects', ProjectsController::class);
+
     Route::post('/projects/{project}/tasks', [ProjectTasksController::class, 'store']);
     Route::patch('/projects/{project}/tasks/{task}', [ProjectTasksController::class, 'update']);
-    Route::post('/projects', [ProjectsController::class, 'store']);
-    Route::delete('/projects/{project}', [ProjectsController::class, 'destroy']);
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
